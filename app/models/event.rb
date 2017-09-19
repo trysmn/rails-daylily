b879f19087b8caf6ce9bce05472ea6e6179f57f5
WimJan Dannijs <wjdannijs@gmail.com>
2017-09-19 13:38:37 +0200

    add both models and relations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,9 @@
 class Event < ApplicationRecord
   belongs_to :user
+
+  has_many :event_categories
+  has_many :categories, through: :event_categories
+
+  has_many :event_terrains
+  has_many :terrains, through: :event_terrains
 end
