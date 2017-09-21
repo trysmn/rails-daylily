d112e8dd04fde2efadbd4681436cbcde745b6128
Trystan Newbury trystan.newbury@gmail.com <trystan.newbury@gmail.com>
2017-09-21 23:43:23 +0200

    add weather scrape
    add min and max temperatures, days of precipitation and total precipitation and monthly sunshine hours

--- app/services/amadeus_api_service.rb
+++ app/services/amadeus_api_service.rb
@@ -1,6 +1,10 @@
+require 'open_weather'
+
 class AmadeusApiService
+
   def search_flights(origin, outbound_date, inbound_date)
     result = HTTParty.get("https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=vTE7KANhuQZ630Qm28BIJ3lSigFrGeus&origin=BOS&destination=LON&departure_date=2017-12-25&number_of_results=1")
     pars_res = result.parsed_response
   end
+
 end
