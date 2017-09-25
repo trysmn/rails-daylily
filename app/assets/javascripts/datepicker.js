a5c8af33b6f44bf3aca1d83505de76ed77f50e50
Dirk Schuler <dscbdv@gmail.com>
2017-09-25 16:42:59 +0200

    Add JS scroll for home

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -3,3 +3,48 @@ $('.datepicker').datepicker({
   format: 'dd/mm/yyyy',
   language: "en"
 });
+
+
+//
+//   $('.datepicker1').datepicker();
+//   $('.datepicker2').datepicker();
+
+//   $(".datepicker1").on("dp.change", function (e) {
+//   $('.datepicker2').data("DatePicker").minDate(e.date);
+//   });
+// });
+
+
+// $( document ).ready(function() {
+
+// $( ".datepicker1" ).datepicker({
+//     minDate: 0,
+//     maxDate: "+2Y",
+//     showWeek: true,
+//     weekHeader: 'Wk',
+//     onSelect: function( selectedDate ) {
+//         $( ".datepicker2" ).datepicker("option", "minDate", selectedDate );
+//         setTimeout(function(){
+//             $( ".datepicker2" ).datepicker('show');
+//         }, 16);
+//     }
+// });
+
+// $( ".datepicker2" ).datepicker({
+//     maxDate: "+2Y",
+//     showWeek: true,
+//     weekHeader: 'Wk'
+// });
+
+// });
+
+
+
+$('.js-anchor-link').click(function(e){
+  e.preventDefault();
+  var target = $($(this).attr('href'));
+  if(target.length){
+    var scrollTo = target.offset().top;
+    $('body, html').animate({scrollTop: scrollTo+'px'}, 800);
+  }
+});
