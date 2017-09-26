d6b7f98cf90a1016c8b4b507ee8d7cb3c12230b3
WimJan Dannijs <wjdannijs@gmail.com>
2017-09-26 13:14:19 +0200

    fix for search in controller

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20170923164118) do
     t.index ["attachinariable_type", "attachinariable_id", "scope"], name: "by_scoped_parent", using: :btree
   end
 
-  create_table "categories", force: :cascade do |t|
+  create_table "categories", id: :bigserial, force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -43,18 +43,18 @@ ActiveRecord::Schema.define(version: 20170923164118) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "event_categories", force: :cascade do |t|
-    t.integer  "category_id"
-    t.integer  "event_id"
+  create_table "event_categories", id: :bigserial, force: :cascade do |t|
+    t.bigint   "category_id"
+    t.bigint   "event_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["category_id"], name: "index_event_categories_on_category_id", using: :btree
     t.index ["event_id"], name: "index_event_categories_on_event_id", using: :btree
   end
 
-  create_table "event_terrains", force: :cascade do |t|
-    t.integer  "terrain_id"
-    t.integer  "event_id"
+  create_table "event_terrains", id: :bigserial, force: :cascade do |t|
+    t.bigint   "terrain_id"
+    t.bigint   "event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_event_terrains_on_event_id", using: :btree
@@ -95,7 +95,7 @@ ActiveRecord::Schema.define(version: 20170923164118) do
     t.index ["city_airport_id"], name: "index_search_details_on_city_airport_id", using: :btree
   end
 
-  create_table "terrains", force: :cascade do |t|
+  create_table "terrains", id: :bigserial, force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
