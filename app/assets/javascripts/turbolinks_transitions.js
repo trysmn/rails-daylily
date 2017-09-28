f028a2c02011851b64fe2cbf43265fe52a011663
Trystan Newbury trystan.newbury@gmail.com <trystan.newbury@gmail.com>
2017-09-28 19:15:59 +0200

    precompiled assets

--- app/assets/javascripts/turbolinks_transitions.js
+++
@@ -1,6 +0,0 @@
-// document.addEventListener('page:change', function() {
-//         document.body.className += 'animated fadeIn';
-// });
-// document.addEventListener('page:fetch', function() {
-//         document.body.className += 'loaded';
-// });
